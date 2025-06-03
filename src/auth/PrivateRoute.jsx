8b84f82ec150881aa87a55cb7baffe7792315577
fabricekalvanda <fabricekalvanda@gmail.com>
2025-06-03 13:56:50 -0700

    add login and signup UI

---
+++ src/auth/PrivateRoute.jsx
@@ -0,0 +1,9 @@
+import { redirect, Route } from "react-router-dom";
+
+export const PrivateRoute = (props) => {
+  const user = null;
+
+  if (!user) return <Redirect to="/login" />;
+
+  return <Route {...props} />;
+};
